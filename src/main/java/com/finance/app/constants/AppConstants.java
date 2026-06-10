d7967d2a07ee31c169990313f67d0cc8571f6af7
msaravananguru <saravanan24121998@gmail.com>
2026-06-10 20:39:38 +0530

    Sprint 0 Completed...

---
+++ src/main/java/com/finance/app/constants/AppConstants.java
@@ -0,0 +1,7 @@
+package com.finance.app.constants;
+
+public class AppConstants {
+
+    public static final Long DEMO_USER_ID = 1L;
+
+}
---
+++ src/main/java/com/finance/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.finance.app.exception;
+
+public class GlobalExceptionHandler {
+
+}
